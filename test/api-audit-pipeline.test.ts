78c88a0d98c77fd12a66eefc9442599e423f4359
Ben Dalgarn <bdalgarn@Bens-MacBook-Air-2.local>
2025-07-08 17:14:35 -0600

    chore: bootstrap CDK project

---
+++ test/api-audit-pipeline.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ApiAuditPipeline from '../lib/api-audit-pipeline-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/api-audit-pipeline-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ApiAuditPipeline.ApiAuditPipelineStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
